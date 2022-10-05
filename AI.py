7ad2bd0bd9385ec8f7c600ab882011ea8e89fa59
BONADA Nathan <nathan.bonada@gmail.com>
2022-10-05 20:26:58 +0200

    added AI file

--- AI.py
+++ AI.py
@@ -5,47 +5,47 @@ from utils import *
 
 class Knowledge():
     """
-    A Knowledge consists of a set of board cells,
-    and a count of the number of those cells which are mines.
-    and another count of the number of those cells which are ships
+    A Knowledge consists of a set of board indexs,
+    and a count of the number of those indexs which are mines.
+    and another count of the number of those indexs which are ships
     """
 
-    def __init__(self, cells, m_count, s_count):
-        self.cells = set(cells)
+    def __init__(self, indexs, m_count, s_count):
+        self.indexs = set(indexs)
         self.mine_count = m_count
         self.ship_count = s_count
 
     def __eq__(self, other):
-        return self.cells == other.cells \
+        return self.indexs == other.indexs \
             and self.mine_count == other.mine_count \
             and self.ship_count == other.ship_count
 
     def __str__(self):
-        return f"{self.cells} : {self.mine_count} mines and {self.ship_count}"
+        return f"{self.indexs} : {self.mine_count} mines and {self.ship_count}"
 
     def known_mines(self):
         """
-        Returns the set of all cells in self.cells known to be mines.
+        Returns the set of all indexs in self.indexs known to be mines.
         """
         pass
 
     def known_safes(self):
         """
-        Returns the set of all cells in self.cells known to be safe.
+        Returns the set of all indexs in self.indexs known to be safe.
         """
         pass
 
-    def mark_mine(self, cell):
+    def mark_mine(self, index):
         """
         Updates internal knowledge representation given the fact that
-        a cell is known to be a mine.
+        a index is known to be a mine.
         """
         raise NotImplementedError
 
-    def mark_safe(self, cell):
+    def mark_safe(self, index):
         """
         Updates internal knowledge representation given the fact that
-        a cell is known to be safe.
+        a index is known to be safe.
         """
 
 
@@ -58,42 +58,52 @@ class PLayerAI(Player):
 
         super.__init__(name,ship_sizes,mine_nb,r_placement)
         
-        # Keep track of cells known to be safe or mines
-        self.mines = set()
-        self.safes = set()
+        # Keep track of indexs known to be safe or mines
+        self.known_mines = set()
+        self.known_safes = set()
+        self.known_ships = set()
 
         # List of sentences about the game known to be true
-        self.knowledge = []
+        self.knowledges = []
 
-    def mark_mine(self, cell):
+    def mark_mine(self, index):
         """
-        Marks a cell as a mine, and updates all knowledge
-        to mark that cell as a mine as well.
+        Marks an tile as a mine, and updates all knowledge
+        to mark that index as a mine as well.
         """
-        self.mines.add(cell)
-        for sentence in self.knowledge:
-            sentence.mark_mine(cell)
+        self.known_mines.add(index)
+        for sentence in self.knowledges:
+            sentence.mark_mine(index)
 
-    def mark_safe(self, cell):
+    def mark_ship(self, index):
         """
-        Marks a cell as safe, and updates all knowledge
-        to mark that cell as safe as well.
+        Marks an tile as a ship, and updates all knowledge
+        to mark that index as a ship as well.
         """
-        self.safes.add(cell)
-        for sentence in self.knowledge:
-            sentence.mark_safe(cell)
+        self.known_ships.add(index)
+        for sentence in self.knowledges:
+            sentence.mark_ship(index)
+    
+    def mark_safe(self, index):
+        """
+        Marks an index as safe, and updates all knowledge
+        to mark that index as safe as well.
+        """
+        self.known_safes.add(index)
+        for sentence in self.knowledges:
+            sentence.mark_safe(index)
 
-    def add_knowledge(self, cell, count):
+    def add_knowledge(self, index, count):
         """
         Called when the Minesweeper board tells us, for a given
-        safe cell, how many neighboring cells have mines in them.
+        safe index, how many neighboring indexs have mines in them.
 
         This function should:
-            1) mark the cell as a move that has been made
-            2) mark the cell as safe
+            1) mark the index as a move that has been made
+            2) mark the index as safe
             3) add a new sentence to the AI's knowledge base
-               based on the value of `cell` and `count`
-            4) mark any additional cells as safe or as mines
+               based on the value of `index` and `count`
+            4) mark any additional indexs as safe or as mines
                if it can be concluded based on the AI's knowledge base
             5) add any new sentences to the AI's knowledge base
                if they can be inferred from existing knowledge
@@ -102,7 +112,7 @@ class PLayerAI(Player):
 
     def make_safe_move(self):
         """
-        Returns a safe cell to choose on the Minesweeper board.
+        Returns a safe index to choose on the Minesweeper board.
         The move must be known to be safe, and not already a move
         that has been made.
 
@@ -114,7 +124,7 @@ class PLayerAI(Player):
     def make_random_move(self):
         """
         Returns a move to make on the Minesweeper board.
-        Should choose randomly among cells that:
+        Should choose randomly among indexs that:
             1) have not already been chosen, and
             2) are not known to be mines
         """
