f7d60e0d73b050f34ea3feccb2d759d6759d9c39
BONADA Nathan <nathan.bonada@gmail.com>
2022-10-07 10:48:02 +0200

    results are saved in a file

--- test_AI_perf.py
+++ test_AI_perf.py
@@ -3,12 +3,15 @@ from AI import PlayerAI
 import time
 
 NB_GAME = 100
-NB_MINE=0
+NB_MINE=8
 SHIP_SIZES=[5,4,3,2]
-
+RESULT_FILE = "resultats_test_perf.txt"
+AI_VERSION = "Prob Map V1"
 game_rounds_list = []
 
 global_start = time.perf_counter()
+
+print("Performance test in progress ...")
 
 for i in range(NB_GAME):
     local_start = time.perf_counter()
@@ -25,8 +28,18 @@ for i in range(NB_GAME):
         
     local_end = time.perf_counter()
     game_rounds_list.append(game.rounds)
-    print("Game {:>3} =======> {:.5f} secs and {} rounds".format(i+1,local_end-local_start,game.rounds))
+    #print("Game {:>3} =======> {:.5f} secs and {} rounds".format(i+1,local_end-local_start,game.rounds))
     
 global_end = time.perf_counter()
-print("Took {} secs to play {} games".format(global_end-global_start,NB_GAME))
-print("Mean : "+str(sum(game_rounds_list)/len(game_rounds_list))+" rounds")
+
+with open(RESULT_FILE, "a") as fichier:
+    fichier.write("====================================\n")
+    fichier.write("AI version : {}\n".format(AI_VERSION))
+    fichier.write("Settings : number of games = {}, ships = {}, mines = {}\n".format(NB_GAME,SHIP_SIZES,NB_MINE))
+    fichier.write("Took {:.5f} secs (mean = {:.5f})\n".format(global_end-global_start,(global_end-global_start)/100))
+    fichier.write("Rounds stats : mean = {:>3}, max = {}, min = {}\n".format(sum(game_rounds_list)/len(game_rounds_list),max(game_rounds_list),min(game_rounds_list)))
+    fichier.write("====================================\n\n")
+    
+
+print("Performance test completed !")
+print("The results have been saved in {}".format(RESULT_FILE))
